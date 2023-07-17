31b7b6ba5f90a56bef50725e426b6cca5f447af2
Tim Vrakas <tim@vrakaslabs.net>
2023-07-16 23:32:34 -0700

    It Transmits!!

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use bsp::board;
 use teensy4_bsp as bsp;
 use teensy4_panic as _;
 
-use core::mem::size_of as size_of;
-use core::ptr::null;
+use core::mem::size_of;
 use core::ptr::addr_of;
+use core::ptr::null;
 
 use bsp::hal::timer::Blocking;
 
@@ -20,11 +20,11 @@ use ral::iomuxc;
 use ral::iomuxc_gpr;
 
 #[repr(C)]
-#[derive(Clone,Copy)]
+#[derive(Clone, Copy)]
 pub struct RxDescriptor {
     len: u16,
     flags: u16,
-    buffer: *const u32
+    buffer: *const u8,
 }
 
 #[repr(C)]
@@ -32,55 +32,86 @@ pub struct RxDescriptor {
 pub struct TxDescriptor {
     len: u16,
     flags: u16,
-    buffer: *const u32
+    buffer: *const u8,
 }
 
 #[repr(C)]
 #[repr(align(64))]
 pub struct RxDT {
-    rxdt: [RxDescriptor; 12]
+    rxdt: [RxDescriptor; 12],
 }
 
 #[repr(C)]
 #[repr(align(64))]
 pub struct TxDT {
-    txdt: [TxDescriptor; 10]
+    txdt: [TxDescriptor; 10],
 }
 
 #[repr(C)]
 #[repr(align(32))]
 pub struct Bufs {
-    tx: [[u32; 128];10],
-    rx: [[u32; 128];12],
+    tx: [[u8; 512]; 10],
+    rx: [[u8; 512]; 12],
 }
 
-static mut RXDT: RxDT  = RxDT{rxdt:[RxDescriptor{len:0, flags:0x8000, buffer:null()};12]};
-static mut TXDT: TxDT  = TxDT{txdt:[TxDescriptor{len:0, flags:0, buffer:null()};10]}; 
+static mut RXDT: RxDT = RxDT {
+    rxdt: [RxDescriptor {
+        len: 0,
+        flags: 0x8000,
+        buffer: null(),
+    }; 12],
+};
+static mut TXDT: TxDT = TxDT {
+    txdt: [TxDescriptor {
+        len: 0,
+        flags: 0,
+        buffer: null(),
+    }; 10],
+};
 
-static mut BUFS: Bufs = Bufs{tx:[[0x0;128];10],rx:[[0x0;128];12]};
+static mut BUFS: Bufs = Bufs {
+    tx: [[0x0; 512]; 10],
+    rx: [[0x0; 512]; 12],
+};
 
-
-fn mdio_write(
-    enet1: &ral::Instance<enet::RegisterBlock, 1>,
-    phyaddr: u8,
-    regaddr: u8,
-    data: u16,
-) {
+fn mdio_write(enet1: &ral::Instance<enet::RegisterBlock, 1>, phyaddr: u8, regaddr: u8, data: u16) {
     ral::write_reg!(enet,enet1,MMFR,ST:1,OP:1,TA:0, PA:phyaddr as u32, RA:regaddr as u32, DATA:data as u32);
-    while ral::read_reg!(enet,enet1,EIR,MII) == 0{}
+    while ral::read_reg!(enet, enet1, EIR, MII) == 0 {}
     ral::write_reg!(enet,enet1,EIR,MII:1);
 }
 
-fn mdio_read(
-    enet1: &ral::Instance<enet::RegisterBlock, 1>,
-    phyaddr: u8,
-    regaddr: u8
-)->u16 {
+fn mdio_read(enet1: &ral::Instance<enet::RegisterBlock, 1>, phyaddr: u8, regaddr: u8) -> u16 {
     ral::write_reg!(enet,enet1,MMFR,ST:1,OP:2,TA:0, PA:phyaddr as u32, RA:regaddr as u32);
-    while ral::read_reg!(enet,enet1,EIR,MII) == 0{}
-    let data = ral::read_reg!(enet,enet1,MMFR,DATA) as u16;
+    while ral::read_reg!(enet, enet1, EIR, MII) == 0 {}
+    let data = ral::read_reg!(enet, enet1, MMFR, DATA) as u16;
     ral::write_reg!(enet,enet1,EIR,MII:1);
     return data;
+}
+
+fn print_dt(delay: &mut Blocking<bsp::hal::gpt::Gpt<1>, 1000>) {
+    log::info!("=== Descriptor Tables ===");
+    //Print the details of all descriptor tables
+    unsafe {
+        delay.block_ms(100); // we were flooding the serial too fast?
+        for (idx, el) in TXDT.txdt.iter().enumerate() {
+            log::info!(
+                "TXDT[{idx:<2}] -- addr: {:#08x}, flags: {:#06x}, len: {:<3}",
+                el.buffer as u32,
+                el.flags,
+                el.len
+            );
+        }
+        delay.block_ms(100); // we were flooding the serial too fast?
+        for (idx, el) in RXDT.rxdt.iter().enumerate() {
+            log::info!(
+                "RXDT[{idx:<2}] -- addr: {:#08x}, flags: {:#06x}, len: {:<3}",
+                el.buffer as u32,
+                el.flags,
+                el.len
+            );
+        }
+    }
+    log::info!("=================");
 }
 
 #[bsp::rt::entry]
@@ -107,7 +138,8 @@ fn main() -> ! {
     gpt1.set_clock_source(GPT1_CLOCK_SOURCE);
 
     // Convenience for blocking delays.
-    let mut delay = Blocking::<_, GPT1_FREQUENCY>::from_gpt(gpt1);
+    let mut delay: Blocking<bsp::hal::gpt::Gpt<1>, 1000> =
+        Blocking::<_, GPT1_FREQUENCY>::from_gpt(gpt1);
 
     //Enable clock distro, both to the ethernet MAC and out the PHY pi
     let ccm1 = unsafe { ral::ccm::CCM::instance() };
@@ -172,14 +204,6 @@ fn main() -> ! {
 
     // //need to enable pins
     // //why no PHY link?
-    // ral::modify_reg!(enet,enet1,RDSR,R_DES_START:0x0); //circular receive buffer descriptor queue
-    // ral::modify_reg!(enet,enet1,TDSR,X_DES_START:0x0); //circular transmit buffer descriptor queue
-    // ral::modify_reg!(enet,enet1,MRBR,R_BUF_SIZE:0x0); //maximum size of all receive buffers
-
-    // ral::modify_reg!(enet,enet1,ECR,ETHEREN:1); //enable
-    // ral::modify_reg!(enet,enet1,RCR,RMII_MODE:1,MII_MODE:1,LOOP:0,PROM:1); //rmii, no loopback, no MAC filter
-    // ral::modify_reg!(enet,enet1,TCR,FDEN:1); //enable full-duplex
-    // ral::modify_reg!(enet,enet1,TFWR,STRFWD:1); // store and fwd
 
     let gpio7 = unsafe { ral::gpio::GPIO7::instance() };
 
@@ -199,14 +223,13 @@ fn main() -> ! {
 
     delay.block_ms(2000);
 
-	mdio_write(&enet1, 0, 0x18, 0x0280); // LED shows link status, active high
-	mdio_write(&enet1, 0, 0x17, 0x0081); // config for 50 MHz clock input
+    mdio_write(&enet1, 0, 0x18, 0x0280); // LED shows link status, active high
+    mdio_write(&enet1, 0, 0x17, 0x0081); // config for 50 MHz clock input
 
     let rcsr = mdio_read(&enet1, 0, 0x17);
     let ledcr = mdio_read(&enet1, 0, 0x18);
     let phycr = mdio_read(&enet1, 0, 0x19);
     log::info!("RCSR:{rcsr}, LEDCR:{ledcr}, PHYCR:{phycr}");
-
 
     // loop {
     //     // mdio_write(&enet1, 0, 0x18, 0x492); // force LED on
@@ -215,7 +238,7 @@ fn main() -> ! {
     //     // delay.block_ms(500);
     // }
 
-    unsafe{
+    unsafe {
         TXDT.txdt[1].len = 0;
         RXDT.rxdt[1].len = 0;
     }
@@ -236,7 +259,7 @@ fn main() -> ! {
     log::info!("TX+RX Buffer size = {buf_size}");
 
     // Initalize all descriptor tables
-    unsafe{
+    unsafe {
         for (idx, element) in TXDT.txdt.iter_mut().enumerate() {
             element.buffer = addr_of!(BUFS.tx[idx][0]);
         }
@@ -250,25 +273,47 @@ fn main() -> ! {
         RXDT.rxdt.last_mut().unwrap().flags = 0x2000;
     }
 
-    //Print the details of all descriptor tables
-    unsafe{
-        for(idx,el) in TXDT.txdt.iter().enumerate(){
-            log::info!("TXDT[{idx}] -- addr: {:#08x}, flags: {:#04x}", el.buffer as u32, el.flags);
-        }
-        delay.block_ms(100); // we were flooding the serial too fast?
-        for(idx,el) in RXDT.rxdt.iter().enumerate(){
-            log::info!("RXDT[{idx}] -- addr: {:#08x}, flags: {:#04x}", el.buffer as u32, el.flags);
-        }
+    print_dt(&mut delay);
+
+    unsafe {
+        ral::write_reg!(enet, enet1, RDSR, addr_of!(RXDT.rxdt[0]) as u32); //circular receive buffer descriptor queue
+        ral::write_reg!(enet, enet1, TDSR, addr_of!(TXDT.txdt[0]) as u32); //circular transmit buffer descriptor queue
+        ral::write_reg!(enet,enet1,MRBR, R_BUF_SIZE:512); //maximum size of all receive buffers
     }
 
-    
+    ral::write_reg!(enet, enet1, EIMR, 0x0); //interupt mask: all off
+    ral::modify_reg!(enet,enet1,RCR,RMII_MODE:1,MII_MODE:1,LOOP:0,PROM:1); //rmii, no loopback, no MAC filter
+    ral::modify_reg!(enet,enet1,ECR,ETHEREN:1, DBSWP:1); //enable, swap bits
+    ral::modify_reg!(enet,enet1,TCR,FDEN:1); //enable full-duplex
+    ral::modify_reg!(enet,enet1,TFWR,STRFWD:1); // store and fwd
 
+    let mut tx_pos: usize = 0;
 
     loop {
-        delay.block_ms(1000); 
-        log::info!("test");
+        delay.block_ms(1000);
+        log::info!("testA");
+        unsafe {
+            log::info!("testB");
+            let desc: &mut TxDescriptor = &mut TXDT.txdt[tx_pos];
+            if (desc.flags & 0x8000) == 0x0 {
+                log::info!("testC");
+                log::info!("tx, num= {tx_pos}");
+                let str = "thisatest";
+                BUFS.tx[tx_pos][0..9].copy_from_slice(str.as_bytes());
+                log::info!("testD");
+                desc.len = 44;
+                desc.flags |= 0x8C00;
+                log::info!("testE");
+                ral::write_reg!(enet,enet1,TDAR,TDAR:1);
+                if tx_pos < 9 {
+                    tx_pos += 1;
+                } else {
+                    tx_pos = 0;
+                }
+                log::info!("testF");
+            }
+        }
     }
-
 }
 
 // We're responsible for configuring our timers.
